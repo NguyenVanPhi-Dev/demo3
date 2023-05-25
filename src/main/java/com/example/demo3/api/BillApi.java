9dcea553505ac74cb73a408fe8586e29402a54cb
Nguyen Van Phi <phi090320@gmail.com>
2023-05-25 14:10:34 +0700

    test git commit

--- src/main/java/com/example/demo3/api/BillApi.java
+++ src/main/java/com/example/demo3/api/BillApi.java
@@ -8,12 +8,9 @@ import com.example.demo3.service.TablesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
